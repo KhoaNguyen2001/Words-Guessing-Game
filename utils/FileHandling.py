722274cd96f04977cbdbb85274d08e37ba64a241
NhanPhamThanh-IT <ptnhanit230104@gmail.com>
2025-08-12 23:44:13 +0700

    Enhance docstring for readJsonFile method to clarify functionality and parameters

--- utils/FileHandling.py
+++ utils/FileHandling.py
@@ -1,9 +1,24 @@
+# File utils/FileHandling.py - Handles file operations
+
 import typing
 import json
 
 class FileHandling:
+    """
+    Handles file operations.
+    """
+
     @staticmethod
     def readJsonFile(filepath: str) -> typing.Optional[list[str]]:
+        """
+        Reads a JSON file and returns the content as a list of strings.
+
+        Args:
+            filepath (str): The path to the JSON file.
+
+        Returns:
+            typing.Optional[list[str]]: A list of strings read from the JSON file, or None if an error occurs.
+        """
         try:
             with open(filepath, 'r') as file:
                 return json.load(file)
