14788381f650e77d78d1c3cdf9bff6c57872eff7
NhanPhamThanh-IT <ptnhanit230104@gmail.com>
2025-08-13 00:03:56 +0700

    Refactor Helper class methods to standardize terminology and improve clarity; rename getRandomFruit to getRandomWord and printFruit to printWord

--- utils/Helper.py
+++ utils/Helper.py
@@ -20,18 +20,18 @@ class Helper:
         return fruits if fruits is not None else []
 
     @staticmethod
-    def getRandomFruit(fruits: list[str]) -> str:
-        return random.choice(fruits)
-    
+    def getRandomWord(words: list[str]) -> str:
+        return random.choice(words)
+
     @staticmethod
     def getIndexOfChar(word: str, char: str) -> list[int]:
         return [i for i, c in enumerate(word) if c == char]
 
     @staticmethod
-    def printFruit(fruit: str, list_index: list[int]) -> None:
-        for i in range(len(fruit)):
+    def printWord(word: str, list_index: list[int]) -> None:
+        for i in range(len(word)):
             if i in list_index:
-                print(fruit[i], end=' ')
+                print(word[i], end=' ')
             else:
                 print('_', end=' ')
         print()
